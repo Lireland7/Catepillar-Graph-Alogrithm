aa4ba8c145fa0011c80807f77b509b60b4586d29
Lireland7 <liam.ireland@me.com>
2022-04-20 11:44:13 -0400

    updated for presentation

--- catbandwidth.py
+++ catbandwidth.py
@@ -1,106 +1,188 @@
 import math
 import this
 
+from pathfinder import Graph
+
+# Read graph in from file
 caterpillar = []
 with open('inputfile') as f:
     contents = f.read()
     print(contents)
     ls = list(contents.split(", "))
     print(ls)
+    # Build tuple to represent caterpillar
     for i in range(0, len(ls)):
         caterpillar.append(int(ls[i]))
 hasChecked = [False for i in caterpillar]
 
 
 def greedyCaterpillarAlgorithm():
-    findLinearEmbedding()
+    print(findLinearEmbedding())
 
 
+# Helper function to find the linear embedding of caterpillar graph
+# Print the bandwidth
+# Print the embedding
 def findLinearEmbedding():
-    bandwidth = bandwidthAlgorithm(findMax())
+    # Get the bandwidth of the caterpillar
+    bandwidth = math.ceil(bandwidthAlgorithm(findMax()))
     print("the bandwidth is:")
-    print(math.floor(bandwidth + 1))
-    linearEmbedding = [0 for i in range(len(caterpillar))]
+    print(math.ceil(bandwidth + 1))
+
+    # Tracks the number of vertices to the left of each spine vertex
+    linearEmbedding = [0 for i in range(len(caterpillar) + 1)]
+
+    # Loop through each spine vertex
     for i in range(len(caterpillar)):
+
+        # Track the remaining space between spine vertices
         remainingSpace = bandwidth - linearEmbedding[i]
+
+        # If there is no remaining space to the left of the spine vertex
+        # Put remaining vertices to the right of the spine
         if caterpillar[i] > remainingSpace:
             caterpillar[i] -= remainingSpace
             linearEmbedding[i] = bandwidth
             linearEmbedding[i + 1] = caterpillar[i]
             caterpillar[i] = 0
+
+        # Else put all the vertices in the space
         else:
             linearEmbedding[i] += caterpillar[i]
             caterpillar[i] = 0
+
     return linearEmbedding
 
 
+# Helper function to find the bandwidth of the graph
+# Parameters:
+#   maxVertex -> the number vertices attached to the max spine vertex or problem vertex
 def bandwidthAlgorithm(maxVertex):
+    # Find the bandwidth of a subgraph centered on the max spine vertex
     bandwidth = findBandwidth(maxVertex)
+
+    # Find other problem vertices in the graph
     possibleProblemNodes = findNextMaxes(bandwidth)
+
     checking = True
     i = 0
+
+    # Checking through list of problem vertices
     while checking:
+
+        # If there are no problem vertices then done
         if len(possibleProblemNodes) == 0:
             checking = False
+
+        # If the bandwidth of the next problem vertex is greater than
+        # the bandwidth of the current vertex
+        # run algorithm on that vertex now
         elif findBandwidth(possibleProblemNodes[i]) > bandwidth:
             bandwidth = bandwidthAlgorithm(possibleProblemNodes[i])
             checking = False
         i = i + 1
+
+        # If all problem vertices have been checked then done
         if i == len(possibleProblemNodes):
             checking = False
+
     return bandwidth
 
 
+# Find the bandwidth of a subgraph
+# Parameters:
+#   startingNode -> The problem vertex to start at
 def findBandwidth(startingNode):
+    # Number of vertices checked to the left and right of the starting vertex
     leftCheckCount = 0
     rightCheckCount = 0
+
+    # Number of spine vertices in subgraph
     spineCount = 1
+    # Number of leaf vertices in subgraph
     leafCount = caterpillar[startingNode]
     calculating = True
+
+    # Formula to calculate bandwidth of subgraph
     bandwidthReturn = (leafCount / (spineCount + 1)) + 1
 
+    # Loop while checking vertices to the left and right of the subgraph
     while calculating:
         bandwidth = (leafCount / (spineCount + 1)) + 1
+
+        # Which node to check on the left and right
         leftCheck = startingNode - leftCheckCount
         rightCheck = startingNode + rightCheckCount
 
+        # If there are no more vertices to check then return bandwidth
         if leftCheck < 0 and rightCheck == len(caterpillar):
             return (leafCount / (spineCount + 1)) + 1
+
+        # If there are no more vertices to check on the left
         elif leftCheck < 0:
+            # If the vertex on the right has more leaf vertices than the bandwidth
+            # Mark spine vertex as checked
+            # Increment counts
             if caterpillar[rightCheck] > bandwidth:
                 hasChecked[rightCheck] = True
                 rightCheckCount = rightCheckCount + 1
                 spineCount = spineCount + 1
                 leafCount += caterpillar[rightCheck]
+
+            # Else done
+            # Return bandwidth
             else:
                 return (leafCount / (spineCount + 1)) + 1
+
+        # If all vertices on the right have been checked
         elif rightCheck == len(caterpillar):
+
+            # If the vertex on the left has more leaf vertices than the bandwidth
+            # Mark spine vertex as checked
+            # Increment counts
             if caterpillar[leftCheck] > bandwidth:
                 hasChecked[leftCheck] = True
                 leftCheckCount += 1
                 spineCount += 1
                 leafCount += caterpillar[leftCheck]
+
+            # Else done
+            # Return Bandwidth
             else:
                 return (leafCount / (spineCount + 1)) + 1
+
+        # Else have to check both
         else:
-            if caterpillar[rightCheck] > caterpillar[leftCheck and caterpillar[rightCheck] > bandwidth]:
+            # If the right vertex has more leaves than the left
+            # And it has more leaves than the bandwidth
+            # Increment counts
+            if caterpillar[rightCheck] > caterpillar[leftCheck] and caterpillar[rightCheck] > bandwidth:
                 hasChecked[rightCheck] = True
                 rightCheckCount += 1
                 spineCount += 1
                 leafCount += caterpillar[rightCheck]
-            elif caterpillar[rightCheck] < caterpillar[leftCheck and caterpillar[leftCheck] > bandwidth]:
+
+            # If the left has more leaves than the right
+            # And it has more leaves than the bandwidth
+            # Increment counts
+            elif caterpillar[rightCheck] < caterpillar[leftCheck] and caterpillar[leftCheck] > bandwidth:
                 hasChecked[leftCheck] = True
                 leftCheckCount += 1
                 spineCount += 1
                 leafCount += caterpillar[leftCheck]
+
+            # Else done
+            # Return bandwidth
             else:
                 return (leafCount / (spineCount + 1)) + 1
     return bandwidthReturn
 
 
+# Find the max Vertex of the graph
 def findMax():
     maxDegreeVertex = 0
     maxDegree = 0
+    # Simple loop to check
     for i in range(len(caterpillar)):
         if caterpillar[i] > maxDegree:
             maxDegreeVertex = i
@@ -108,12 +190,20 @@ def findMax():
     return maxDegreeVertex
 
 
+# Find all problem vertices
+# Parameters:
+#   bandwidth -> current bandwidth estimate
 def findNextMaxes(bandwidth):
     maximums = []
+    # SImple loop to check
     for i in range(len(caterpillar)):
         if caterpillar[i] > bandwidth and hasChecked[i] == False:
             maximums.append(i)
     return maximums
+
+
+
+
 
 
 greedyCaterpillarAlgorithm()
--- pathfinder.py
+++ pathfinder.py
@@ -1,28 +1,113 @@
-graph = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['F'],
-    'D': [],
-    'E': ['F'],
-    'F': []
-}
-
-visited = []
-queue = []
+def printPath(stack):
+    for i in range(len(stack) - 1):
+        print(stack[i], end=" -> ")
+    print(stack[-1])
 
 
-def bfs(visited, graph, node):
-    visited.append(node)
-    queue.append(node)
+# Graph class with breadth first search and depth first search
+# algorithms implemented to find the longest path of graph
+# graph implemented in undirected
+class Graph:
 
-    while queue:
-        s = queue.pop(0)
-        print(s, end=" ")
+    # Constructor Method
+    # Parameters:
+    #   vertices -> number of vertices in graph
+    # Globals:
+    #   vertices -> number of vertices in graph
+    #   graph -> edge set
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.graph = {i: [] for i in range(self.vertices)}
 
-        for i in graph[s]:
-            if i not in visited:
-                visited.append(i)
-                queue.append(i)
+    # Method to add edges to edge set
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    # Breadth first search implementation.
+    # Parameters:
+    #   node -> the current vertex
+    def bfs(self, node):
+
+        queue = []
+        visited = [False for i in range(self.vertices + 1)]
+        distance = [-1 for i in range(self.vertices + 1)]
+        visited[node] = True
+        queue.append(node)
+        distance[node] = 0
+
+        while queue:
+            s = queue.pop(0)
+            print(s, end=" ")
+
+            for i in self.graph[s]:
+                if not visited[i]:
+                    visited[i] = True
+                    distance[i] = distance[s] + 1
+                    queue.append(i)
+
+        maxDistance = 0
+
+        for i in range(self.vertices):
+            if distance[i] > maxDistance:
+                maxDistance = distance[i]
+                node = i
+
+        print("\n")
+        print(maxDistance)
+        return node, maxDistance
+
+    # Depth first search implementation
+    # Finds the vertices in the longest path
+    # Parameters:
+    #   visited -> what vertices have already been visited
+    #   current -> current vertex dfs is on
+    #   end -> end vertex of the path
+    #   stack -> stack to track the path
+    def dfs(self, visited, current, end, stack):
+        # Add current to the path
+        stack.append(current)
+        if current == end:
+            printPath(stack)
+            return stack
+        visited[current] = True
+
+        if len(self.graph[current]) > 0:
+            for next in self.graph[current]:
+
+                # if the node is not visited
+                if not visited[next]:
+                    self.dfs(visited, next, end, stack)
+
+        del stack[-1]
+
+    def longestPath(self):
+        stack = []
+        longestNode = 0
+        longestDistance = 0
+        for i in range(self.vertices):
+            if len(self.graph[i]) == 1:
+                node, dist = self.bfs(i)
+                if dist > longestDistance:
+                    longestNode = i
+                    longestDistance = dist
+
+        node, LongDistance = self.bfs(longestNode)
+        visited = [0 for i in range(self.vertices)]
+        path = self.dfs(visited, longestNode, node, stack)
+        print('Longest path is from', longestNode, 'to', node, 'of length', LongDistance)
+        return path
 
 
-bfs(visited, graph, 'A')
+# Driver
+G = Graph(10)
+G.addEdge(0, 1)
+G.addEdge(1, 2)
+G.addEdge(2, 3)
+G.addEdge(2, 9)
+G.addEdge(2, 4)
+G.addEdge(4, 5)
+G.addEdge(1, 6)
+G.addEdge(6, 7)
+G.addEdge(6, 8)
+path = G.longestPath()
